bc6031d897bdb695ee7796d402c6c2d4f5ef2b92
David Mentler <david.mentler@shapr3d.com>
2025-04-06 12:37:23 +0200

    Fix Expose loading wrong modules

--- src/lldb-imgui/src/Expose.cpp
+++ src/lldb-imgui/src/Expose.cpp
@@ -5,6 +5,7 @@
 #include "lldb/API/SBDebugger.h"
 
 #include <mach-o/dyld.h>
+#include <mach-o/loader.h>
 #include <mach-o/getsect.h>
 #include <mach-o/ldsyms.h>
 
@@ -15,6 +16,39 @@ namespace lldb::imgui {
 
 static void LogAdapter(const char* message, void* baton) {
     spdlog::info("[SelfDebugger] {}", message);
+}
+
+static std::optional<std::string> GetImageUUID(void* imageBase) {
+    auto headerAddr = reinterpret_cast<uintptr_t>(imageBase);
+
+    auto header = reinterpret_cast<struct mach_header_64*>(headerAddr);
+    if (header->magic != MH_MAGIC_64) {
+        return std::nullopt;
+    }
+
+    auto cmdAddr = headerAddr + sizeof(struct mach_header_64);
+
+    for (uint32_t cmdIndex = 0; cmdIndex < header->ncmds; cmdIndex++) {
+        auto* command = reinterpret_cast<struct load_command*>(cmdAddr);
+
+        if (command->cmd == LC_UUID) {
+            auto* command = reinterpret_cast<struct uuid_command*>(cmdAddr);
+
+            std::string uuid;
+
+            for (uint32_t i = 0; i < sizeof(command->uuid); i++) {
+                if (i == 4 || i == 6 || i == 8 || i == 10) {
+                    uuid.append("-");
+                }
+                uuid.append(std::format("{:02X}", command->uuid[i]));
+            }
+            return uuid;
+        }
+
+        cmdAddr += command->cmdsize;
+    }
+
+    return std::nullopt;
 }
 
 static SBTarget CreateSelfReflection() {
@@ -39,7 +73,13 @@ static SBTarget CreateSelfReflection() {
             return;
         }
 
-        auto mod = target.AddModule(info.dli_fname, nullptr, nullptr);
+        auto uuid = GetImageUUID(info.dli_fbase);
+        if (!uuid) {
+            spdlog::error("Failed to determine UUID for '{}'", label);
+            return;
+        }
+
+        auto mod = target.AddModule(info.dli_fname, nullptr, uuid->c_str());
 
         for (auto i = 0; i < mod.GetNumSections(); i++) {
             auto segment = mod.GetSectionAtIndex(i);
@@ -58,8 +98,15 @@ static SBTarget CreateSelfReflection() {
         }
     };
 
-    loadModuleWithAddr("executable", dlsym(RTLD_MAIN_ONLY, MH_EXECUTE_SYM));
+    // Always reflect on LLDB - having this first makes it the preferred source of symbols
     loadModuleWithAddr("lldb", (void*) SBDebugger::Initialize);
+
+    // Reflect on host executable if it isn't us
+    if (auto* header = dlsym(RTLD_MAIN_ONLY, MH_EXECUTE_SYM)) {
+        if (header != &_mh_execute_header) {
+            loadModuleWithAddr("host", header);
+        }
+    }
 
     return target;
 }
@@ -67,14 +114,19 @@ static SBTarget CreateSelfReflection() {
 void* Expose(const char* name) {
     static SBTarget self = CreateSelfReflection();
 
-    SBSymbolContextList list = self.FindSymbols(name);
-    if (list.GetSize() != 1) {
-        return nullptr;
+    for (uint32_t i = 0; i < self.GetNumModules(); i++) {
+        auto mod = self.GetModuleAtIndex(i);
+
+        auto symbol = mod.FindSymbol(name);
+        if (!symbol.IsValid()) {
+            continue;
+        }
+
+        addr_t loadAddr = symbol.GetStartAddress().GetLoadAddress(self);
+        return reinterpret_cast<void*>(loadAddr);
     }
 
-    SBSymbol symbol = list.GetContextAtIndex(0).GetSymbol();
-    addr_t loadAddr = symbol.GetStartAddress().GetLoadAddress(self);
-    return reinterpret_cast<void*>(loadAddr);
+    return nullptr;
 }
 
 }
